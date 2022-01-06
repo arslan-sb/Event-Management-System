c577cdc15e685d7e7144e8acaab6273201298630
Arslan <01-134211-053@student.bahria.edu.pk>
2022-01-06 22:56:22 +0500

    Made seperate folders for different classes

--- Event class/Event.h
+++ Event class/Event.h
@@ -1,18 +1,23 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include "Date.h"
+#include "Time.h"
 using namespace std;
 
 class Event {
 protected:
 	string name;
+	Date heldOn;
+	Time startTime;
 	int ID;
 	int participants;
 
 public:
-	Event() :name(), ID(0), participants(0) {}
-	Event(string n, int id, int part) :name(n), ID(id), participants(part) {}
+	Event() :name(), ID(0), participants(0), heldOn(), startTime() {}
+	Event(string n, Date held, Time start, int id, int part) :name(n), heldOn(held), startTime(start), ID(id), participants(part) {}
 	void setName(string n) { this->name = n; }
+
 	string getName() { return name; }
 	void setID(int ID) { this->ID = ID; }
 	int getID() { return ID; }
